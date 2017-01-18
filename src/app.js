645b561c688b02a2fcdb17fe0cf0aefed0cfe521
Cody Averett <codyaverett@gmail.com>
2017-01-17 22:50:23 -0600

    Changing default port to 3001

--- src/app.js
+++ src/app.js
@@ -6,27 +6,28 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 app.use(logger('development'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(__dirname));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 if (app.get("env== 'development'")) {
-    app.listen(3000, function () {
-        console.log('Example listening on port 3000!');
-    });
-}
-else {
-    app.listen(8080, function () {
-        console.log('Example listening on port 8080!');
-    });
+  app.listen(3001, function () {
+    console.log('Example listening on port 3001!');
+  });
+} else {
+  app.listen(8080, function () {
+    console.log('Example listening on port 8080!');
+  });
 }
 module.exports = app;
